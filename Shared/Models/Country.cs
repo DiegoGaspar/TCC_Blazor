97429953f5e9432a75ed549647cc7ffeb7b1ad5b
Diego Gaspar <diego_gaspar25@hotmail.com>
2021-06-03 21:56:33 -0300

    Comite inicial

---
+++ Shared/Models/Country.cs
@@ -0,0 +1,11 @@
+using System.Collections.Generic;
+
+namespace appblazor.Shared.Models
+{
+     public class Country
+    {
+        public string name { get; set; }
+        public string alpha2Code { get; set; }
+        public string capital { get; set; }
+    }
+}
